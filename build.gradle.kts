f27c4b3f81f7a4811d72f1a5548cf3328455f23f
Filip Stojakovic <filip.stojakovic1@gmail.com>
2025-11-10 19:16:40 +0100

    update gitignore

---
+++ build.gradle.kts
@@ -0,0 +1,65 @@
+plugins {
+    id("java")
+    id("org.jetbrains.kotlin.jvm") version "2.1.0"
+    id("org.jetbrains.intellij.platform") version "2.7.1"
+}
+
+group = "com.example"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
+}
+
+// Configure IntelliJ Platform Gradle Plugin
+// Read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin.html
+dependencies {
+    intellijPlatform {
+        create("IC", "2025.1.4.1")
+        testFramework(org.jetbrains.intellij.platform.gradle.TestFrameworkType.Platform)
+
+        // Add necessary plugin dependencies for compilation here, example:
+        // bundledPlugin("com.intellij.java")
+    }
+}
+
+intellijPlatform {
+    pluginConfiguration {
+        ideaVersion {
+            sinceBuild = "251"
+        }
+
+        changeNotes = """
+            Initial version
+        """.trimIndent()
+    }
+}
+
+tasks {
+    patchPluginXml {
+        sinceBuild.set("241")
+        untilBuild.set("999.*")
+    }
+
+    buildPlugin {
+        archiveFileName.set("timestamp-converter.zip")
+    }
+
+    runIde {
+        jvmArgs("-Xmx1g")
+    }
+    // Set the JVM compatibility versions
+    withType<JavaCompile> {
+        sourceCompatibility = "21"
+        targetCompatibility = "21"
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
+    }
+}
